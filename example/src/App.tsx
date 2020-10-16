27e4f3aac7b82c25e88685874641cc649ee2eccc
borris wiria <borris.wiria@123rf.com>
2020-10-16 23:41:40 +0700

    first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'test'
+import { Header } from 'test'
 import 'test/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Header text="Create React Library Example 😄" />
 }
 
 export default App
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { Header } from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(Header).toBeTruthy()
   })
 })
